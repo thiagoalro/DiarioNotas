51ddcc2d16b21711153a1d957ecefa4d3aaf6363
thiago <thiago.alves@ifce.edu.br>
2023-01-23 19:25:28 -0300

    cria classe GradeBook com atributos e métodos get e set do nomeCurso

---
+++ src/GradeBook.java
@@ -0,0 +1,15 @@
+public class GradeBook {
+	private String nomeCurso;
+	double[] notasAlunos;
+
+	public String getNomeCurso(){
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso){
+		this.nomeCurso = nomeCurso;
+	}
+
+
+
+}
